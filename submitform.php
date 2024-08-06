a6d0634e4cb1b9fb6a2eab9ff1233696850a5a8c
rusestudio <59683457+rusestudio@users.noreply.github.com>
2024-08-07 01:19:53 +0900

    submitform.php

---
+++ submitform.php
@@ -0,0 +1,29 @@
+<?php
+
+//connect to database
+$host = "localhost";
+$username = "root";
+$password = "gandaksa33";
+$database = "rusestudio";
+$conn = new mysqli($host, $username, $password, $database); //must inorder like above
+
+//check if connect
+if ($conn-> connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+
+// get data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+$comment = $_POST['comment'];
+
+//insert data to database table
+$sql = "INSERT INTO form_tbl (name,email,country,comment) VALUES ('$name', '$email', '$country', '$comment')";
+if ($conn->query($sql)===TRUE){
+	echo "Form Submitted Successfully";
+}
+else {
+	echo "Error: ".$sql."<br>".$conn->error;
+}
+?>
