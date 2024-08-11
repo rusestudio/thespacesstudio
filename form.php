5ef6b1855517964fdf5a1c4b4fb5e4af7c732b33
rusestudio <59683457+rusestudio@users.noreply.github.com>
2024-08-12 01:26:13 +0900

    Update form.php

--- form.php
+++ form.php
@@ -1,26 +1,31 @@
+?>
 <?php
+// database connection code
+if(isset($_POST['name']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','rusestudio');
 
-//connect to database
-$conn = mysqli_connect('localhost', 'root', '','rusestudio');
+// get the post records
 
-//check if connect
-if ($conn-> connect_error){
-	die("Connection failed: " . $conn->connect_error);
-}
-
-// get data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $country = $_POST['country'];
 $comment = $_POST['comment'];
 
-//insert data to database table
-$sql = "INSERT INTO `form_tbl` (`Id`, `name`, `email`, `country`, `comment`) VALUES ( '0', $name', '$email', '$country', '$comment')";
-if ($conn->query($sql)===TRUE){
-	echo "Form Submitted Successfully";
+// database insert SQL code
+$sql = "INSERT INTO `tbl_contact` (`id`, `name`, `email`, `country`, `comment`) VALUES ('0', '$name', '$email', '$country', '$comment')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Contact Records Inserted";
 }
 }
-else {
-	echo "Error: ".$sql."<br>".$conn->error;
+else
+{
+	echo "Are you a genuine visitor?";
+	
 }
 ?>
